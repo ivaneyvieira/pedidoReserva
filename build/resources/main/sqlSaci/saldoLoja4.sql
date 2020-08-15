3b2b9113e9f30eb500091c79bc085581111dd7b8
ivaneyvieira <ivaneyvieira@gmail.com>
2020-08-15 09:11:29 -0300

    Removing ignored files

--- build/resources/main/sqlSaci/saldoLoja4.sql
+++
@@ -1,7 +0,0 @@
-SELECT storeno,
-       prdno,
-       qtty_varejo / 1000 AS saldo
-FROM sqldados.stk
-WHERE storeno = 4
-  AND prdno = :prdno
-  AND grade = :grade
--- build/resources/main/sqlSaci/updateUser.sql
+++
@@ -1,4 +0,0 @@
-UPDATE sqldados.users AS U
-SET bits2    = :bitAcesso,
-    auxLong1 = :storeno
-WHERE no = :no
